5058b957c12555f818f8ba2789c159bbcbe1a9ea
jasuni69 <jasonnicolini@outlook.com>
2024-09-28 14:01:44 +0200

    weatherapp

---
+++ weatherapp.py
@@ -0,0 +1,24 @@
+import requests
+from config import WEATHERSTACK_API_KEY
+import tkinter as tk
+
+
+def get_weather(location: str):
+    api_key = WEATHERSTACK_API_KEY
+    url = f"https://api.weatherstack.com/current?access_key={api_key}"
+    querystring = {"query": location}
+    response = requests.get(url, params=querystring)
+    data = response.json()
+
+  
+    data_list = data["current"]
+    temperature = data_list["temperature"]
+    weather_description = data_list["weather_descriptions"][0]
+    print(f"The weather in {location} is {weather_description}")
+    print(f"The temperature in {location} is {temperature} C°")
+
+place = input("Where do you want to see the weather? For example Stockholm or London.")
+get_weather(location=place)
+
+
+
